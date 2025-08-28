0d61f886e244e79e70c986d3ac9b66a7ed222ec9
Andrey <adreygichan@gmail.com>
2025-08-28 13:05:53 +0300

    добавлено crud для товаров

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(products.router)
 
 
 @app.get("/")
---
+++ app/routers/products.py
@@ -0,0 +1,64 @@
+from fastapi import APIRouter, Depends, HTTPException, status, Response
+from sqlalchemy.orm import Session
+from .. import models, schemas, utils
+from ..database import get_db
+
+router = APIRouter(prefix="/products", tags=["Products"])
+
+@router.get("/", response_model=list[schemas.ProductBase])
+def get_products(db: Session = Depends(get_db)):
+    products = db.query(models.Product).all()
+    return products
+
+
+@router.get("/{id}", response_model=schemas.ProductBase)
+def get_product(id: int, db: Session = Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.id == id).first()
+
+    if not product:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Товар с id: {id} не был найден",
+        )
+    return product
+
+
+@router.post("/", response_model=schemas.Product, status_code=status.HTTP_201_CREATED)
+def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
+    new_product = models.Product(**product.model_dump())
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    return new_product
+
+
+@router.put("/{id}", response_model=schemas.Product)
+def update_product(id: int, updated_product: schemas.ProductCreate, db: Session = Depends(get_db)):
+    product_query = db.query(models.Product).filter(models.Product.id == id)
+    product =  product_query.first()
+
+    if not product:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Товар с id: {id} не был найден",
+        )
+    
+    product_query.update(updated_product.model_dump(), synchronize_session=False)  # type: ignore
+    db.commit()
+    return product_query.first()
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_product(id: int, db: Session = Depends(get_db)):
+    product_query = db.query(models.Product).filter(models.Product.id == id)
+    product = product_query.first()
+
+    if product == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Товар с id: {id} не существует",
+        )
+
+    product_query.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    
